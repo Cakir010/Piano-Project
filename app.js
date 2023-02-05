75b63af87e980e576552236b34d02fc21ff166ed
emre cakir <ynsckr01@gmail.com>
2023-02-05 10:55:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key')
+volumeSlider  = document.querySelector('.volume-slider input')
+keysCheckbox = document.querySelector('.keys-checkbox input')
+let allKeys = [],
+audio= new Audio('tunes/tunes/a.wav');
+
+const playTune = (key) => {
+    audio.src= `tunes/tunes/${key}.wav`;
+audio.play();
+
+
+const clickedKey = document.querySelector('[data-key=${key}]')
+clickedKey.classList.add('active');
+setTimeout(()=> {
+    clickedKey.classList.remove('active')
+
+},150)
+}
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key)
+    key.addEventListener('click' , ()=> playTune(key.dataset.key))
+    
+})
+
+const handleVolume = (e) => {
+    audio.volume = e.target .value;
+}
+const showHideKeys = ()=>{
+    pianoKeys.forEach(key => key.classList.toggle('hide'))
+}
+
+const pressedKey = (e)=>{
+   if(allKeys.includes(e.key)) playTune(e.key);
+}
+
+
+volumeSlider.addEventListener('input' , handleVolume)
+document.addEventListener('keydown' , pressedKey)
+keysCheckbox.addEventListener('click', showHideKeys)
+
+
+
+
+
+
+
+
+
+
